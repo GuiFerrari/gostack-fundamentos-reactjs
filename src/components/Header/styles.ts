b6587a8a7bdec59a247a76789597591f82958d17
GuiFerrari <guiferrari10@hotmail.com>
2020-05-04 20:14:00 -0300

    Finalized project

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,28 +7,37 @@ interface ContainerProps {
 export const Container = styled.div<ContainerProps>`
   background: #5636d3;
   padding: 30px 0;
-
   header {
+    max-width: 100%;
     width: 1120px;
     margin: 0 auto;
     padding: ${({ size }) => (size === 'small' ? '0 20px ' : '0 20px 150px')};
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     nav {
       a {
+        position: relative;
         color: #fff;
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
-
         & + a {
           margin-left: 32px;
         }
-
+        opacity: 0.6;
         &:hover {
-          opacity: 0.6;
+          opacity: 1;
+        }
+        &:hover:after {
+          content: '';
+          position: absolute;
+          bottom: -10px;
+          left: 0;
+          opacity: 1;
+          width: 100%;
+          height: 100%;
+          border-bottom: 2px solid #ff872c;
         }
       }
     }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -37,10 +37,24 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      await api.get('transactions').then(res => {
-        setTransactions(res.data.transactions);
-        setBalance(res.data.balance);
-      });
+      const response = await api.get('/transactions');
+
+      const transactionsFormatted = response.data.transactions.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue: formatValue(transaction.value),
+          formattedDate: formatDate(transaction.created_at),
+        }),
+      );
+
+      const balanceFormatted = {
+        income: formatValue(response.data.balance.income),
+        outcome: formatValue(response.data.balance.outcome),
+        total: formatValue(response.data.balance.total),
+      };
+
+      setTransactions(transactionsFormatted);
+      setBalance(balanceFormatted);
     }
 
     loadTransactions();
@@ -67,30 +81,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">
-              R$
-              {` ${formatValue(Number(balance.income))}`}
-            </h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">
-              R$
-              {` ${formatValue(Number(balance.outcome))}`}
-            </h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">
-              R$
-              {` ${formatValue(Number(balance.total))}`}
-            </h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -111,15 +116,14 @@ const Dashboard: React.FC = () => {
                   <tr key={transaction.id}>
                     <td className="title">{transaction?.title}</td>
                     <td className={transaction.type}>
-                      {transaction.type === 'outcome' && '- '}
-                      R$
-                      {` ${formatValue(Number(transaction.value))}`}
+                      {transaction.type === 'outcome' && ' - '}
+                      {transaction.formattedValue}
                     </td>
                     <td className="category">
                       {showIcon(transaction.category?.title)}
                       <span>{transaction.category?.title}</span>
                     </td>
-                    <td>{formatDate(transaction.created_at)}</td>
+                    <td>{transaction.formattedDate}</td>
                   </tr>
                 );
               })}
